4b148c3c1fbf018622aca5aac7acf9eb5ed40382
christendeo <christen.deocampo@gmail.com>
2026-02-23 22:56:15 -0500

    Implemented a hoc for managing window statws and add the first window - Terminal

--- src/constants/index.js
+++ src/constants/index.js
@@ -102,27 +102,23 @@ const blogPosts = [
 const techStack = [
     {
         category: "Frontend",
-        items: ["React.js", "Next.js", "TypeScript"],
+        items: ["HTML", "CSS", "React.js", "Next.js"],
     },
     {
-        category: "Mobile",
-        items: ["React Native", "Expo"],
+        category: "Backend",
+        items: ["Node.js", "Express", "Redis", "NestJS"],
     },
     {
         category: "Styling",
         items: ["Tailwind CSS", "Sass", "CSS"],
     },
     {
-        category: "Backend",
-        items: ["Node.js", "Express", "NestJS", "Hono"],
-    },
-    {
         category: "Database",
-        items: ["MongoDB", "PostgreSQL"],
+        items: ["MongoDB", "PostgreSQL", "GraphQL"],
     },
     {
         category: "Dev Tools",
-        items: ["Git", "GitHub", "Docker"],
+        items: ["Git", "GitHub", "Docker", "ElasticSearch"],
     },
 ];
 

893de8ed0bdc6af29224194da382192d922b677d
christendeo <christen.deocampo@gmail.com>
2026-04-08 22:26:04 -0400

    Fix PDF viewer

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -7,10 +7,9 @@ import {useState} from "react";
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    "pdfjs-dist/build/pdf.worker.min.mjs",
-    import.meta.url,
-).toString();
+
+// Ensures PDF viewer works on public domain
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const Resume = () => {
 
